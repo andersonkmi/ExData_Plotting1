c05b2a471fe840ca7dc4be9b1ab5461e2139960a
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-10-02 15:18:33 -0300

    Call to unzip

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 library(dplyr)
 
 # Unzips the data set
-#unzip("household_power_consumption.zip", "household_power_consumption.txt")
+unzip("household_power_consumption.zip", "household_power_consumption.txt")
 power_consumption <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
 power_feb <- filter(power_consumption,Date %in% c("1/2/2007","2/2/2007")) 
 
