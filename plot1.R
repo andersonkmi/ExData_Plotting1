e0ff848a1ebe447470b6da0e76ea5af511308dc7
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-10-02 16:06:57 -0300

    Modified variable name

--- plot1.R
+++ plot1.R
@@ -5,10 +5,10 @@ unzip("household_power_consumption.zip", "household_power_consumption.txt")
 
 # Loads the data set
 power_consumption <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
-power_feb <- filter(power_consumption,Date %in% c("1/2/2007","2/2/2007")) 
+scoped_power_data <- filter(power_consumption,Date %in% c("1/2/2007","2/2/2007")) 
 
 png("plot1.png",width=480,height=480)
-hist(power_feb$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
+hist(scoped_power_data$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
 dev.off()
 
 
