8f4b7f4eb30b454b86a73e10e42396776ccd88a6
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2016-10-02 15:16:45 -0300

    Add initial files

---
+++ plot1.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+# Unzips the data set
+#unzip("household_power_consumption.zip", "household_power_consumption.txt")
+power_consumption <- read.csv("household_power_consumption.txt", sep=";", header=TRUE,stringsAsFactors=FALSE,na.strings="?")
+power_feb <- filter(power_consumption,Date %in% c("1/2/2007","2/2/2007")) 
+
+png("plot1.png",width=480,height=480)
+hist(power_feb$Global_active_power,xlab="Global Active Power (kilowatts)", main="Global Active Power",c="red")
+dev.off()
+
+
